144f433c6765b5e5f23d8d40a237a4c1639b9440
onsyedes <ons.yedes@tek-up.de>
2024-05-30 12:39:55 +0200

    creating reservations microservice + docker file + docker compose

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+//useFactory is going to tell nest how it is going to create the mongoose module options
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {
+  /* abstracting the schema initialization process away from implementation */
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
