363a1903452fee8aed2a66280b4004e837875721
onsyedes <ons.yedes@tek-up.de>
2024-06-01 13:10:32 +0200

    Passport JWT Strategy

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../current-user.decorator';
+import { UsersDocument } from './models/user.schema';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -8,5 +11,10 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getAuthenticatedUser(@CurrentUser() user: UsersDocument) {
+    return user;
   }
 }
